b34e79e32f37b8c7c06e895370e576f69edf4323
jmmtra <jmmtra@gmail.com>
2013-10-30 17:14:05 +0100

    First prototype

--- tcf2vrt.py
+++ tcf2vrt.py
@@ -1,134 +1,142 @@
-#!/usr/bin/python
+#!/usr/bin/env python -w
 # -*- coding: utf-8 -*-
 
-############################
-#
-#    tcf_wrapper.py
-#    by Jörg Didakowski
-#
-############################
+# refactor Jörg's reader in a simple ElementTree API
 
-# Vorsicht, hier funktionieren die Namespaces noch nicht.
-# das muss noch implementiert werden !
-
-# der aufruf des seminer-Programms muss noch angepasst werden (call_seminer/0)
-
-import cgi
-import string, sys, os
-import re
-from read_tcf_format_v_0_4 import READ_TCF_FORMAT_v_0_4
-import codecs
-
-###########################################################################
-# klasse die den TCF-Wrapper für Seminer implementiert
-###########################################################################
-class TCF_WRAPPER:
+try:
+    from lxml import etree # if we don't need any of the fancy things provided by lxml we should drop it, although it does not hurt
+    print("running with lxml.etree")
+except importError:
+    try:
+        import xml.etree.cElementTree as etree
+        print("running with cElementTree")
+    except importError:
+        import xml.etree.ElementTree as etree
+        print("running with ElementTree")
         
-    def __init__ (self):
-        # nach belieben Konfigurieren
-        self.m_strFileInterfaceIn = 'seminer_in.txt'
-        self.m_strFileInterfaceOut = 'seminer_out.txt'
-        self.m_strSeminerModel = 'raw'    # 'full'
-
-        self.m_vvToken = []
-        self.m_vvTokenId = []
-        self.m_vvNeClass = []
-        self.m_strRequest = ""
-        pass
-
-    # aufrufen des Programms seminer
-    def call_seminer (self):
-        # os.system("./seminer %s <%s >%s" % (self.m_strSeminerModel,self.m_strFileInterfaceIn,self.m_strFileInterfaceOut) )
-        # os.system("echo hier noch den seminer-Aufruf einfügen")
-        os.system("/Users/jmmmac/resources/sequor-0.3.0/bin/seminer %s <%s >%s" % (self.m_strSeminerModel,self.m_strFileInterfaceIn,self.m_strFileInterfaceOut) )
-
-    # integrieren der NE-Information in das TCF-XML
-    def merge_information (self, clarinxml, insert):
-        sresult = re.search("</[\w]*:?TextCorpus>", clarinxml)
-        pos = sresult.start()
-        return clarinxml[0:pos] + insert + clarinxml[pos:]
-
-    # formatieren der Token als seminer-Eingabeformat
-    def token_2_seminer (self, vvToken):
-        vDummy1 = []
-        for i in vvToken:
-            vDummy2 = []
-            for j in i:
-                vDummy2.append(j)
-                vDummy2.append('\n')
-            if len(vDummy1) > 0:
-                vDummy1.append('\n')
-            vDummy1.append(''.join(vDummy2))
-        return ''.join(vDummy1)
-
-    # extrahieren der NE-Klassifizierungen aus der seminer-Ausgabe
-    def seminer_2_neclass (self, fileIn):
-        vNeClass = []
-        vvNeClass = []
-        vLine = fileIn.readlines()
-        for i in vLine:
-            strLine = i.rstrip('\n')
-            if strLine == "":
-                vvNeClass.append(vNeClass)
-                vNeClass = []
-            else:
-                vNeClass.append(strLine)
-        return vvNeClass
-
-    # formatieren von tcf-entity
-    def format_tcf_entity (self, vvNeClass, vvTokenIds):
-        vTCF = []
-        iIdCounter = 0
-        for i in range(0, len(vvNeClass)):
-            for j in range(0, len(vvNeClass[i])):
-                if vvNeClass[i][j] != "O":
-                    vTCF.append("<entity ID=\"%s\" class=\"%s\" tokenIDs=\"%s\"/>" % ("ne_" + str(iIdCounter), vvNeClass[i][j], vvTokenIds[i][j]))
-                    iIdCounter += 1
-        return ''.join(vTCF)
-
-    # formatieren von tcf-entities
-    def format_tcf_entities (self, strEntity):
-        return "<namedEntities type=\"CoNLL2002\">" + strEntity + "</namedEntities>"
-
-    # wrapper um seminer wird aufgerufen
-    def process(self, strFileTcfIn, strFileTcfOut):
-        self.read_tcf(strFileTcfIn)
-        self.write_seminer_input(self.m_strFileInterfaceIn)
-        self.call_seminer()
-        self.read_seminer_output(self.m_strFileInterfaceOut)
-        self.write_tcf(strFileTcfOut)
-
-    # einlesen des tcf-Formats
-    def read_tcf (self, strFilename):
-        fileTcfIn = codecs.open(strFilename, 'r', 'utf8')
-        self.m_strRequest = ''.join(fileTcfIn.readlines())
-        fileTcfIn.close()
-        data_v_0_4 = READ_TCF_FORMAT_v_0_4()
-        data_v_0_4.parse_string(self.m_strRequest.encode('utf8'))
-        (self.m_vvToken, self.m_vvTokenId) = data_v_0_4.get_tokens_and_ids()
-
-    # schreiben der seminer-Eingabe
-    def write_seminer_input (self, strFilename):
-        strSeminerInput = self.token_2_seminer(self.m_vvToken)
-        fileInterfaceOut = codecs.open(strFilename, 'w', 'utf8')
-        fileInterfaceOut.write(strSeminerInput)
-        fileInterfaceOut.close()
-
-    # lesen der Seminer-Ausgabe
-    def read_seminer_output (self, strFilename):
-        fileInterfaceIn = codecs.open(strFilename, 'r', 'utf8')
-        self.m_vvNeClass = self.seminer_2_neclass(fileInterfaceIn)
-        fileInterfaceIn.close()
-
-    # schreiben der tcf-Datei
-    def write_tcf (self, strFilename):
-        strTcfEntities = self.format_tcf_entities (self.format_tcf_entity(self.m_vvNeClass, self.m_vvTokenId))
-        fileOut = codecs.open(strFilename, 'w', 'utf8')
-        fileOut.write(self.merge_information (self.m_strRequest, strTcfEntities))
-        fileOut.close()
+import re # To use regular expressions
 
 
-### MAIN ###############################
+#===============================================================================
+# Read the input file
+#===============================================================================
 
-wrapper = TCF_WRAPPER()
-wrapper.process ("./in2.tcf", "./out2.tcf")
+# input variable as command line argument
+
+# output variable as command line argument
+
+# read the file in a SAX-like style to reduce memory consumption to a minimum?
+# Does semiNER wrapper do that or it is just a matter of style?
+
+# check which tcf layers are present. This can be done at the very beginning or
+# per function/element manipulation. Or handle exceptions with try. Because if
+# the elements are no present we will get an error and the script will be
+# aborted.
+
+tcf = etree.parse("result7_2794771265787765018.xml")
+
+#===============================================================================
+# Add positional attributes to tokens
+#===============================================================================
+
+# declare a namespace to avoid typing the whole URI again and again?
+
+for token in tcf.findall('//{http://www.dspin.de/data/textcorpus}token'):
+    ID = token.attrib["ID"]
+    # pos
+    tag = tcf.find('//{}tag[@tokenIDs="{}"]'.format('{http://www.dspin.de/data/textcorpus}',ID))
+    token.attrib["pos"] = tag.text
+    # lemma
+    lemma = tcf.find('//{}lemma[@tokenIDs="{}"]'.format('{http://www.dspin.de/data/textcorpus}',ID))
+    token.attrib["lemma"] = lemma.text
+
+
+#===============================================================================
+# Construct the output tree
+#===============================================================================
+
+# what value should be given to the id attribute? The name of the file without
+# the extension? And checking that is a valid token (no strange characters and
+# starting with an alphabetic character)
+
+text = etree.Element('text', attrib={'id':"text1"})
+
+tcf_sentences = tcf.findall('//{http://www.dspin.de/data/textcorpus}sentence')
+
+for sentence in tcf_sentences:
+    s = etree.SubElement(text, "s")
+    s.attrib["ID"] = sentence.attrib["ID"]
+    s_tokenIDs = sentence.attrib["tokenIDs"].split(" ")
+    for i in s_tokenIDs:
+        word =  tcf.find('//{}token[@ID="{}"]'.format('{http://www.dspin.de/data/textcorpus}',i))
+        word.tag = "token"
+        s.append(word)
+
+xml = etree.ElementTree(text)
+
+#===============================================================================
+# Save the output as a XML file
+#===============================================================================
+
+# xml.write("out.xml", encoding="utf8", pretty_print=True, xml_declaration=True, method="xml")
+
+#===============================================================================
+# Save the output as a VRT file
+#===============================================================================
+
+"""
+sentences = xml.findall('//s')
+
+for i in sentences:
+    tokens = []
+    for j in i:
+        tokens.append(j.text+"\t"+j.attrib["pos"]+"\t"+j.attrib["lemma"]+"\t"+j.attrib["ID"])
+        j.getparent().remove(j)
+    i.text = '\n'.join(tokens)
+    
+# solution below is better, once structural attributes are added
+# getparent() is only available in lxml, good for me and server, but bad dependency for students
+"""
+
+tokens = xml.findall('//token')
+
+for token in tokens:
+    string = token.text+"\t"+token.attrib["pos"]+"\t"+token.attrib["lemma"]+"\t"+token.attrib["ID"] # convert token into string
+    parent = xml.find('//token[@ID="{}"]..'.format(token.attrib["ID"])) # get the parent element
+    if parent.text == None:
+        parent.text = string
+    else:
+        parent.text = "\n".join([parent.text,string])
+    parent.remove(token)
+
+# Is there a way to avoid writing to a file and do the last block on the fly?
+
+xml.write("out.vrt", encoding="utf8", xml_declaration=True, method="xml")
+
+# the following is to prettify the xml for encoding with cwb
+
+with open("out.vrt", mode='r+') as vrt:# do we need the encoding?
+    text = vrt.read()
+    text = re.sub(r"><", r">\n<", text)
+    text = re.sub(r">([^<\n])", r">\n\1", text)
+    text = re.sub(r"([^\n])<", r"\1\n<", text)
+    vrt.seek(0)
+    vrt.write(text)
+
+
+#xml.write("out.xml",encoding="utf-8")
+    
+    # this could be saved for each sentence, to retrieve the corresponding tokens 
+    #s = etree.Element('s', attrib={'ID':item.attrib["ID"],'start':tokenIDs[0],'end':tokenIDs[-1]})
+    #print(s.tag, s.attrib["ID"], s.attrib["start"], s.attrib["end"])
+
+"""
+        self.tokenIDs = item.attrib[1].split(" ")
+        self.sentstart = item.tokenIDs[0]
+        self.sentend = item.tokenIDs[-1]
+"""
+
+#===============================================================================
+# Save into
+#===============================================================================
+
