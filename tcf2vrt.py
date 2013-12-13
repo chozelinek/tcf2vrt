fb8205ad77802084055612850789e50b3304f131
jmmtra <jmmtra@gmail.com>
2013-12-13 16:30:32 +0100

    Basic implementation for word, lemma, pos and sentence attributes.

--- tcf2vrt.py
+++ tcf2vrt.py
@@ -1,56 +1,130 @@
 #!/usr/bin/env python -w
 # -*- coding: utf-8 -*-
 
-# refactor Jörg's reader in a simple ElementTree API
+#===============================================================================
+#    tcf2vrt.py
+#    by José Manuel Martínez Martínez
+#===============================================================================
+
+import sys
+import codecs # to handle properly unicode
+import re # to use regular expressions
+import argparse # to parse command-line arguments 
+
+#===============================================================================
+# Parse command-line arguments
+#===============================================================================
+
+parser = argparse.ArgumentParser()
+parser.add_argument("infile", help="path to the input file in TCF format")
+parser.add_argument("outfile", help="path to the output file in VRT format")
+args = parser.parse_args()
+
+infile = args.infile
+outfile = args.outfile
+
+#===============================================================================
+# Import XML module module
+#===============================================================================
 
 try:
     from lxml import etree # if we don't need any of the fancy things provided by lxml we should drop it, although it does not hurt
-    print("running with lxml.etree")
+    print "running with lxml.etree"
 except importError:
     try:
         import xml.etree.cElementTree as etree
-        print("running with cElementTree")
+        print "running with cElementTree"
     except importError:
         import xml.etree.ElementTree as etree
-        print("running with ElementTree")
-        
-import re # To use regular expressions
-
+        print "running with ElementTree"
 
 #===============================================================================
 # Read the input file
 #===============================================================================
 
-# input variable as command line argument
-
-# output variable as command line argument
-
-# read the file in a SAX-like style to reduce memory consumption to a minimum?
-# Does semiNER wrapper do that or it is just a matter of style?
-
 # check which tcf layers are present. This can be done at the very beginning or
 # per function/element manipulation. Or handle exceptions with try. Because if
 # the elements are no present we will get an error and the script will be
-# aborted.
+# aborted. IMPORTANT
 
-tcf = etree.parse("result7_2794771265787765018.xml")
+# For an off-line version we might want to convert only a few layers (specify
+# them, etc), for an on-line version we want to convert all layers contained in
+# a tcf file at a particular point of the pipeline
+
+# I think the easiest approach is to check which layers are there and convert
+# them if we have an algorithm for it. If not, just ignore the layer.
+
+# generate kind of metadata, like which tools have been used, or which tagset has been used
+
+# create functions whenever it is posible
+
+# infile = sys.argv[1] # input file
+# outfile = sys.argv[2] # output file
+
+with codecs.open(infile, encoding='utf-8', mode='r') as input:
+    tree = etree.parse(input)
+
+# declare a namespace to avoid typing the whole URI again and again?
+dspin = "{http://www.dspin.de/data/textcorpus}"
+
+layers = tree.find('{}TextCorpus'.format(dspin)) # We get the element that contains all the annotation layers in the file
+
+print len(layers) # How many layers we have
+
+for child in layers:
+    print child.tag,'\t', child.attrib # Print all layers and attributes
+
+# we need to classify the different layers as positional attribute or structural attribute, do we need some kind of dictionary with this information?
 
 #===============================================================================
 # Add positional attributes to tokens
 #===============================================================================
 
-# declare a namespace to avoid typing the whole URI again and again?
+# The following block adds to each token element an attribute per each layer containing positional information
 
-for token in tcf.findall('//{http://www.dspin.de/data/textcorpus}token'):
+# this function works as long as the tcf element contains the token ID as
+# attribute and the text/content is the info to be taken as attrib. For example:
+# lemma, tag. Moreover, every token has always its counterpart in the particular
+# layer. For some layers, that will be the case, for other layers won't. Some
+# checking has to be introduced, like in norm layer.
+
+# we might need a method to process each layer if necessary, and transform the
+# information in something that can be used by add_positional_attrib
+
+# May we need to define a class for attributes, an attribute has to contain the
+# tcf nomenclature, our vrt nomenclature, and if they are positional or
+# structural? Is it enough to have a kind of data structure containing all this
+# information?
+
+def add_positional_attrib(id,element,attribute): 
+    positional_element = tree.find('//{}{}[@tokenIDs="{}"]'.format(dspin,element,id))
+    token.attrib[attribute] = positional_element.text
+    
+positional_elements = [("lemma","lemma"), ("tag","pos")] # list of tuples: each
+# list element is a layer/positional attribute, each element has two values: the
+# name of the element in tcf and the name of the attribute in vrt
+
+for token in tree.findall('//{}token'.format(dspin)):
     ID = token.attrib["ID"]
+    for item in positional_elements:
+        add_positional_attrib(ID,item[0],item[1])
+    """
     # pos
-    tag = tcf.find('//{}tag[@tokenIDs="{}"]'.format('{http://www.dspin.de/data/textcorpus}',ID))
+    tag = tree.find('//{}tag[@tokenIDs="{}"]'.format(dspin,ID))
     token.attrib["pos"] = tag.text
     # lemma
-    lemma = tcf.find('//{}lemma[@tokenIDs="{}"]'.format('{http://www.dspin.de/data/textcorpus}',ID))
+    lemma = tree.find('//{}lemma[@tokenIDs="{}"]'.format(dspin,ID))
     token.attrib["lemma"] = lemma.text
-
-
+    """
+    """
+    # norm
+    norm = tree.find('//{}correction[@tokenIDs="{}"]'.format(dspin,ID))
+    if norm == None:
+        token.attrib["norm"] = token.text
+    else:
+        token.attrib["norm"] = norm.text
+    print(token.attrib["norm"])
+    """
 #===============================================================================
 # Construct the output tree
 #===============================================================================
@@ -61,14 +135,29 @@ for token in tcf.findall('//{http://www.dspin.de/data/textcorpus}token'):
 
 text = etree.Element('text', attrib={'id':"text1"})
 
-tcf_sentences = tcf.findall('//{http://www.dspin.de/data/textcorpus}sentence')
+# create a general function to reconstruct the information as structural inline
+# XML
 
-for sentence in tcf_sentences:
+"""
+structural_elements = [("sentence","s", "text")] # list of tuples: each list element is
+# a structural attribute, each element has three values: the name of the element
+# in tcf, the name of the attribute in vrt, and the parent
+"""
+
+# we need also a structure, at least the parent of the structural element to be constructed
+
+# since there are only a few potential structural attributes it might be better
+# to have ad hoc functions for each of this structures, since each of this
+# structural attributes will be a different challenge.
+    
+tree_sentences = tree.findall('//{}sentence'.format(dspin))
+
+for sentence in tree_sentences:
     s = etree.SubElement(text, "s")
     s.attrib["ID"] = sentence.attrib["ID"]
     s_tokenIDs = sentence.attrib["tokenIDs"].split(" ")
     for i in s_tokenIDs:
-        word =  tcf.find('//{}token[@ID="{}"]'.format('{http://www.dspin.de/data/textcorpus}',i))
+        word = tree.find('//{}token[@ID="{}"]'.format(dspin,i))
         word.tag = "token"
         s.append(word)
 
@@ -84,24 +173,10 @@ xml = etree.ElementTree(text)
 # Save the output as a VRT file
 #===============================================================================
 
-"""
-sentences = xml.findall('//s')
-
-for i in sentences:
-    tokens = []
-    for j in i:
-        tokens.append(j.text+"\t"+j.attrib["pos"]+"\t"+j.attrib["lemma"]+"\t"+j.attrib["ID"])
-        j.getparent().remove(j)
-    i.text = '\n'.join(tokens)
-    
-# solution below is better, once structural attributes are added
-# getparent() is only available in lxml, good for me and server, but bad dependency for students
-"""
-
 tokens = xml.findall('//token')
 
 for token in tokens:
-    string = token.text+"\t"+token.attrib["pos"]+"\t"+token.attrib["lemma"]+"\t"+token.attrib["ID"] # convert token into string
+    string = "\t".join([token.text,token.attrib["pos"],token.attrib["lemma"],token.attrib["ID"]]) # convert token into string
     parent = xml.find('//token[@ID="{}"]..'.format(token.attrib["ID"])) # get the parent element
     if parent.text == None:
         parent.text = string
@@ -109,34 +184,11 @@ for token in tokens:
         parent.text = "\n".join([parent.text,string])
     parent.remove(token)
 
-# Is there a way to avoid writing to a file and do the last block on the fly?
+vrt = etree.tostring(xml, encoding='unicode', method='xml') # convert the XML tree into a string to manipulate it
+vrt = re.sub(r"><", r">\n<", vrt)
+vrt = re.sub(r">([^<\n])", r">\n\1", vrt)
+vrt = re.sub(r"([^\n])<", r"\1\n<", vrt)
 
-xml.write("out.vrt", encoding="utf8", xml_declaration=True, method="xml")
+vrt = etree.ElementTree(etree.fromstring(vrt)) # parse the string as an element and convert the element in a tree
 
-# the following is to prettify the xml for encoding with cwb
-
-with open("out.vrt", mode='r+') as vrt:# do we need the encoding?
-    text = vrt.read()
-    text = re.sub(r"><", r">\n<", text)
-    text = re.sub(r">([^<\n])", r">\n\1", text)
-    text = re.sub(r"([^\n])<", r"\1\n<", text)
-    vrt.seek(0)
-    vrt.write(text)
-
-
-#xml.write("out.xml",encoding="utf-8")
-    
-    # this could be saved for each sentence, to retrieve the corresponding tokens 
-    #s = etree.Element('s', attrib={'ID':item.attrib["ID"],'start':tokenIDs[0],'end':tokenIDs[-1]})
-    #print(s.tag, s.attrib["ID"], s.attrib["start"], s.attrib["end"])
-
-"""
-        self.tokenIDs = item.attrib[1].split(" ")
-        self.sentstart = item.tokenIDs[0]
-        self.sentend = item.tokenIDs[-1]
-"""
-
-#===============================================================================
-# Save into
-#===============================================================================
-
+vrt.write("out.vrt", encoding="utf8", xml_declaration=True, method="xml") # write the result to a file
